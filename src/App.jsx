c0ba182c7e78ebbba515f86198f7c135e8da6060
VolTorian <33360939+VolTorian@users.noreply.github.com>
2024-10-28 22:13:51 -0700

    ability to shuffle items (with a button)
    button for testing purposes only, will be removed later

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import './App.css'
 import Card from './Card'
 
 function App() {
-    const items = [
+    const initialItems = [
         {name: "apple", id: crypto.randomUUID(), src: ""},
         {name: "banana", id: crypto.randomUUID(), src: ""},
         {name: "kiwi", id: crypto.randomUUID(), src: ""},
@@ -12,9 +12,22 @@ function App() {
         {name: "strawberry", id: crypto.randomUUID(), src: ""},
         {name: "peach", id: crypto.randomUUID(), src: ""},
     ]
+    const [items, setArray] = useState(initialItems);
+
+    function shuffleArray() {
+        const nextArr = [...initialItems];
+        for (let i = nextArr.length - 1; i >= 0; i--) {
+            let j = Math.floor(Math.random() * (i + 1));
+            let temp = nextArr[i];
+            nextArr[i] = nextArr[j];
+            nextArr[j] = temp;
+        }
+        setArray(nextArr);
+    }
 
     return (
         <>
+            <button onClick={shuffleArray}>Shuffle</button>
             <div id="board">
                 {items.map((item) => {
                     return <Card name={item.name} key={item.id} src={item.src}/>
